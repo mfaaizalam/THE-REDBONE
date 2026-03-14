b6f868bfdbb1613491a291835ab9f7bd7f10b067
mfaaizalam <mfaaiz2000bce@gmail.com>
2026-03-15 00:11:31 +0500

    deploy

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"https://the-redbone-7xuw.vercel.app//api",
+    baseURL:"https://the-redbone-7xuw.vercel.app/api",
     // baseURL:"http://localhost:5000/api",
       withCredentials:true,
 })
