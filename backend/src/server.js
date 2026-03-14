d81a1b6bfdea4b9aff1c4d0f0249334798534065
mfaaizalam <mfaaiz2000bce@gmail.com>
2026-03-14 23:57:02 +0500

    deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://goldenhorde.vercel.app", //frontend url
+      "https://the-redbone.vercel.app", //frontend url
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
